c4409715eb391fd23597a69fdfaaaa408943a5d6
Jonathan Gruber <gruberjonathan@gmail.com>
2018-02-01 02:10:52 +0000

    Improved file upload modal

--- src/components/OpenFileModal.js
+++ src/components/OpenFileModal.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Typography, Modal, Button } from 'material-ui';
-import { blueGrey } from 'material-ui/colors';
+import { blueGrey, red, green } from 'material-ui/colors';
 import FolderOpenIcon from 'material-ui-icons/FolderOpen';
+import CheckCircleIcon from 'material-ui-icons/CheckCircle';
+import NotInterestedIcon from 'material-ui-icons/NotInterested';
 import Dropzone from 'react-dropzone';
 
 import { getModalStyle } from '../util/style';
@@ -20,44 +22,58 @@ const styles = theme => ({
     position: 'fixed',
     width: theme.spacing.unit * 60,
     backgroundColor: blueGrey[50],
-    boxShadow: theme.shadows[5],
+    boxShadow: theme.shadows[6],
     color: blueGrey[900],
     outline: 'none !important',
   },
   headline: {
     background: theme.palette.secondary.dark,
-    marginBottom: theme.spacing.unit * 2.5,
     padding: `${theme.spacing.unit * 1.5}px ${theme.spacing.unit * 3}px`,
   },
-});
-
-const dropStyles = {
-  base: {
+  withPadding: {
+    padding: theme.spacing.unit * 3,
+    paddingTop: 0,
+  },
+  drop: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
     minHeight: 120,
     margin: '1.5rem',
-    borderWidth: 2,
-    borderStyle: 'dashed',
-    borderColor: blueGrey[500],
-    backgroundColor: 'transparent',
-    transition: 'all .3s ease-in-out',
+    backgroundColor: blueGrey[100],
+    transition: 'background-color .3s ease-in-out',
   },
-  active: {
-    borderColor: blueGrey[700],
-    background: blueGrey[100],
+  dropActive: {
+    backgroundColor: blueGrey[100],
   },
-};
+  dropAccept: {
+    background: green[300],
+  },
+  dropReject: {
+    background: red[300],
+  },
+});
 
 class OpenFileModal extends React.Component {
   state = {
-    open: true,
+    open: false,
+    accepted: null,
+    rejected: null,
   };
 
-  onFileDrop = (files) => {
-    console.log(files);
+  onFileDropAccepted = (files) => {
+    this.setState({
+      accepted: files,
+      rejected: null,
+    });
+  };
+
+  onFileDropRejected = (files) => {
+    this.setState({
+      accepted: null,
+      rejected: files,
+    });
   };
 
   openModal = () => {
@@ -78,8 +94,8 @@ class OpenFileModal extends React.Component {
         </Button>
 
         <Modal
-          open={this.state.open}
           onClose={this.closeModal}
+          open={this.state.open}
           BackdropProps={{
             classes: { root: classes.backdrop },
           }}
@@ -89,14 +105,51 @@ class OpenFileModal extends React.Component {
               Load XML file
             </Typography>
             <Dropzone
-              onDrop={this.onFileDrop}
-              style={dropStyles.base}
-              activeStyle={dropStyles.active}
+              onDropAccepted={this.onFileDropAccepted}
+              onDropRejected={this.onFileDropRejected}
+              accept="text/xml"
+              preventDropOnDocument
+              disablePreview
+              className={classes.drop}
+              activeClassName={classes.dropActive}
+              acceptClassName={classes.dropAccept}
+              rejectClassName={classes.dropReject}
             >
               <Typography type="subheading" style={{ color: 'inherit' }}>
-                Drop an XML file here or click for a dialogue
+                Drop an XML file here or click for a file dialogue
               </Typography>
             </Dropzone>
+
+            {
+              this.state.accepted &&
+              <Typography type="body1" className={classes.withPadding} style={{ color: green[600] }}>
+                <CheckCircleIcon
+                  style={{
+                    position: 'relative',
+                    top: 7,
+                    marginRight: 8,
+                  }}
+                />
+                <span><code>{this.state.accepted.map(f => f.name)}</code> succesfully loaded!</span>
+              </Typography>
+            }
+
+
+            {
+              this.state.rejected &&
+              <Typography type="body1" className={classes.withPadding} style={{ color: red[600] }}>
+                <NotInterestedIcon
+                  style={{
+                    position: 'relative',
+                    top: 7,
+                    marginRight: 8,
+                  }}
+                />
+                <span><code>{this.state.rejected.map(f => f.type)}</code> is not allowed!</span>
+              </Typography>
+            }
+
+
           </div>
         </Modal>
       </div>
